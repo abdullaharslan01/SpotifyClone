06b24e6322ff90fb687c9419768c3d3735ac48b8
Abdullah <abdullaharslan1473@gmail.com>
2024-10-31 09:36:54 +0300

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by abdullah on 31.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
